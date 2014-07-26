4da8980d16099df2692fb61c2bae9ef587861d04
Vsevolod Kukol <sevo@sevo.org>
2014-07-26 22:51:50 +0200

    [GTK] optimize Gtk.Justification conversion readability

--- Xwt.Gtk/Xwt.GtkBackend/Conversion.cs
+++ Xwt.Gtk/Xwt.GtkBackend/Conversion.cs
@@ -154,22 +154,22 @@ namespace Xwt.GtkBackend
 
 		public static Gtk.Justification ToGtkValue (this Alignment value)
 		{
-			if (value == Alignment.Center)
-				return Gtk.Justification.Center;
-			if (value == Alignment.Start)
-				return Gtk.Justification.Left;
-			// if (value == Alignment.End)
-			return Gtk.Justification.Right;
+			switch (value) {
+				case Alignment.Start: return Gtk.Justification.Left;
+				case Alignment.Center: return Gtk.Justification.Center;
+				case Alignment.End: return Gtk.Justification.Right;
+			}
+			throw new NotSupportedException ();
 		}
 
 		public static Alignment ToXwtValue (this Gtk.Justification value)
 		{
-			if (value == Gtk.Justification.Center)
-				return Alignment.Center;
-			if (value == Gtk.Justification.Left)
-				return Alignment.Start;
-			// if (value == Gtk.Justification.Right)
-			return Alignment.End;
+			switch (value) {
+				case Gtk.Justification.Left: return Alignment.Start;
+				case Gtk.Justification.Center: return Alignment.Center;
+				case Gtk.Justification.Right: return Alignment.End;
+			}
+			return Alignment.Start; // return Start for unknown and Fill
 		}
 	}
 }
