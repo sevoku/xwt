a0630b461c9d6daad78802add2318e6244028e48
Vsevolod Kukol <sevo@xamarin.com>
2016-09-19 14:18:25 +0200

    [Gtk3] Fix build

--- Xwt.Gtk/Xwt.GtkBackend/GtkWorkarounds.cs
+++ Xwt.Gtk/Xwt.GtkBackend/GtkWorkarounds.cs
@@ -1378,14 +1378,22 @@ namespace Xwt.GtkBackend
 
 			using (var attr = iter.SafeGetCopy (Pango.AttrType.Foreground)) {
 				if (attr != null) {
+					#if XWT_GTK3
+					tag.Foreground = ((Pango.AttrForeground)attr).Color.ToString();
+					#else
 					tag.Foreground = ((Gdk.PangoAttrEmbossColor)attr).Color.ToString ();
+					#endif
 					result = true;
 				}
 			}
 
 			using (var attr = iter.SafeGetCopy (Pango.AttrType.Background)) {
 				if (attr != null) {
+				#if XWT_GTK3
+					tag.Foreground = ((Pango.AttrBackground)attr).Color.ToString();
+					#else
 					tag.Background = ((Gdk.PangoAttrEmbossColor)attr).Color.ToString ();
+					#endif
 					result = true;
 				}
 			}
